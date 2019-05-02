1a96a56da9861e506d392cd19c3efdd03b16eaab
ince01 <handcock.dk@gmail.com>
2019-05-01 19:52:48 -0700

    add component

--- app/containers/App/App.js
+++ app/containers/App/App.js
@@ -16,24 +16,25 @@ import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 import LoginPage from '../LoginPage/Loadable';
+import ChatPage from '../ChatPage/ChatPage'
 import './style.scss';
 
 const App = () => (
   <div className="app-wrapper">
-    <Helmet
+    {/* <Helmet
       titleTemplate="%s - Nore Chatting"
       defaultTitle="React.js Boilerplate"
     >
       <meta name="description" content="A React.js Boilerplate application" />
     </Helmet>
-    <Header />
+    <Header /> */}
     <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route path="/login" component={LoginPage} />
+      <Route path="/chat" component={ChatPage} />
       <Route path="/features" component={FeaturePage} />
       <Route path="" component={NotFoundPage} />
     </Switch>
-    <Footer />
+    {/* <Footer /> */}
   </div>
 );
 
